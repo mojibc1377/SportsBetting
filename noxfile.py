2ea5b2faffee1ed4085806e235b78735b2d94058
Mojtaba Bagheri Chenari <mojibc1377@gmail.com>
2024-05-05 20:28:57 +0300

    initial commit

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,7 @@ from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
 from typing import Any
 
+
 import nox
 
 os.environ.update({'PDM_IGNORE_SAVED_PYTHON': '1'})
